433a0c0f07e77d86a2382d6566633ff8d2e3bc80
Bhargav <bhargav@getpostman.com>
2016-04-20 16:57:14 +0530

    created all api wrapper calls and documented them

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# pls
+# MyFYBA Private Listing API - PHP Library
+
 PLS API - MYFYBA Private Listing System API
+
+## Installation
+
+Via Composer: Here's what you add to your composer.json file to have `src/API.php` automatically imported into your vendors folder:
+
+    {
+        "require": {
+            "myfyba/pls": "dev-master"
+        }
+    }
+
+Of course, you'll then need to run `composer update`.  
+
+## How to use
+
+#### Note: All methods return serialized JSON
+
+### Instantiate API class
+
+    use MyFYBA\PLS\API;
+    
+    $settings = [
+        'key' => 'YOUR API KEY' (String, Required), // Example: 'dcabf94ad97d6f09914c8be1a302891520a5964d'
+        'id' => USER ID (Number, Required), // Example: 56
+        'endpoint' => 'https://api.myfyba.org' (Optional)
+    ];
+    
+    $api = new API($settings);
+
+### Filter vessels
+
+Method: `vessel('filter string')`
+
+    $api->vessel('page=1&currency=usd&price=0,1000000');
+    
+Optionally `get_filters()` method returns complete query string, hence we can rewrite the above call as
+    
+    $api->vessel($api->get_filters());
+    
+    
+### Fetch vessel using it's ID
+
+Method: `vessel(id)`
+    
+    $api->vessel(82);
+    
+### Filter charter vessels
+
+Method: `charter('filter string')`
+
+    $api->charter('page=1&currency=usd&price=0,1000000');
+    $api->charter($api->get_filters()); // As explained above
+    
+    
+### Fetch charter details using vessel ID
+
+Method: `charter(id)`
+    
+    $api->charter(82);
+
+
+### Fetch brokerage details (Yours)
+
+Method: `brokerage()`
+
+    $api->brokerage();
+    
+### Fetch brokerage by it's ID
+
+Method: `brokerage(id)`
+
+    $api->brokerage(5);
--- src/API.php
+++ src/API.php
@@ -1,10 +1,29 @@
 <?php
 
+/**
+ * PLS API : PHP wrapper for the v1.0 MyFYBA PLS API
+ *
+ * PHP version 5.5 and above
+ *
+ * @category Private API
+ * @package  MyFYBA PLS API
+ * @author   Bhargav Nanekalva <bhargav3@gmail.com>
+ * @license  MIT License
+ * @version  1.0.4
+ * @link     http://github.com/myfyba/pls
+ */
+
 namespace MyFYBA\PLS;
 
 use Curl\Curl;
 
 class API {
+
+    /**
+     * API constructor.
+     * @param $settings
+     * @throws
+     */
     function __construct($settings) {
 
         if (!in_array('curl', get_loaded_extensions())) {
@@ -13,13 +32,65 @@ class API {
 
         $this->key = $settings['key'];
         $this->id = $settings['id'];
+        if (empty($settings['endpoint'])) {
+            $this->endpoint = 'https://api.yachtmls.pro';
+        } else {
+            $this->endpoint = $settings['endpoint'];
+        }
+
         $this->curl = new Curl();
         $this->curl->setHeader('X-API-KEY', $this->key);
         $this->curl->setHeader('X-CLIENT-ID', $this->id);
     }
 
-    public function vessel($host = "https://api.yachtmls.pro", $filters = null, $id = null) {
-        $url = $host . '/vessel';
-        return $this->curl->get($url);
+    /**
+     * @param $filters
+     * @param $slug
+     * @return string
+     */
+    private function url($filters, $slug) {
+        $url = $this->endpoint;
+
+        if (!empty($filters) && !is_numeric($filters)) {
+            $url = $this->endpoint . '/' . $slug . '?' . $filters;
+        } elseif (!empty($filters) && is_numeric($filters)) {
+            $url = $this->endpoint . '/' . $slug . '/' . $filters;
+        } elseif (!empty($slug)) {
+            $url = $this->endpoint . '/' . $slug;
+        }
+
+        return $url;
     }
+
+    /**
+     * @return mixed
+     */
+    public function get_filters() {
+        return $_SERVER["QUERY_STRING"];
+    }
+
+    /**
+     * @param $filters
+     * @return string
+     */
+    public function vessel($filters) {
+        return $this->curl->get($this->url($filters, 'vessel'));
+    }
+
+    /**
+     * @param $filters
+     * @return string
+     */
+    public function charter($filters) {
+        return $this->curl->get($this->url($filters, 'charter'));
+    }
+
+    /**
+     * @param null $id
+     * @return string
+     */
+    public function brokerage($id = null) {
+        return $this->curl->get($this->url($filters = $id, 'brokerage'));
+    }
+
 }
